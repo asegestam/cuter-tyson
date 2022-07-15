ecbb35b2f40cd9ea99af6bc014f42839fc57e739
Albin Segestam <albin.segestam@squeed.com>
2022-07-15 16:15:44 +0200

    Changed to webp and added nuxt/image

--- components/Tyson.vue
+++ components/Tyson.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center">
-    <img class="
+    <NuxtImg class="
       w-full
       h-full
       sm:w-[30vw]
@@ -8,9 +8,8 @@
       rounded-lg 
       shadow-lg
     shadow-black
-    object-cover
     animate-fade-in
-    duration-300" :src="tyson.imgPath" />
+    duration-300" fit="cover" format="webp" :src="tyson.imgPath" />
     <button
       class="px-4 py-2 rounded mt-10 bg-slate-300 hover:bg-slate-400 hover:scale-105 disabled:bg-slate-500 disabled:cursor-wait"
       :disabled="disabled" @click="$emit('votedFor')">
