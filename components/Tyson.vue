c595c8190e7ee4d0b7a1ccccf29f150bc285b205
Albin Segestam <albin.segestam@squeed.com>
2022-07-15 17:13:20 +0200

    Revert to img

--- components/Tyson.vue
+++ components/Tyson.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center">
-    <NuxtImg class="
+    <!-- <NuxtImg class="
       w-full
       h-full
       sm:w-[30vw]
@@ -9,7 +9,18 @@
       shadow-lg
     shadow-black
     animate-fade-in
-    duration-300" fit="cover" format="webp" :src="tyson.imgPath" />
+    duration-300" fit="cover" format="webp" :src="tyson.imgPath" /> -->
+    <img class="
+      w-full
+      h-full
+      sm:w-[30vw]
+      sm:h-[65vh]
+      object-cover
+      rounded-lg 
+      shadow-lg
+    shadow-black
+    animate-fade-in
+    duration-300" :src="tyson.imgPath" />
     <button
       class="px-4 py-2 rounded mt-10 bg-slate-300 hover:bg-slate-400 hover:scale-105 disabled:bg-slate-500 disabled:cursor-wait"
       :disabled="disabled" @click="$emit('votedFor')">
