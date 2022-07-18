314ba17f1e13fc42509b29e756070a18c7d8c13f
Albin Segestam <albin.segestam@squeed.com>
2022-07-19 00:11:00 +0200

    Fix for vercel again

--- components/Tyson.vue
+++ components/Tyson.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center">
-    <nuxt-img class="
+    <!-- <nuxt-img class="
       w-full
       h-full
       sm:w-[30vw]
@@ -10,8 +10,8 @@
       object-cover
     shadow-black
     animate-fade-in
-    duration-300" quality="80" format="webp" :src="tyson.imgPath" />
-    <!-- <img class="
+    duration-300" quality="80" format="webp" :src="tyson.imgPath" /> -->
+    <img class="
       w-full
       h-full
       sm:w-[30vw]
@@ -21,7 +21,7 @@
       shadow-lg
     shadow-black
     animate-fade-in
-    duration-300" :src="tyson.imgPath" /> -->
+    duration-300" :src="tyson.imgPath" />
     <button
       class="px-4 py-2 rounded mt-10 bg-slate-300 hover:bg-slate-400 hover:scale-105 disabled:bg-slate-500 disabled:cursor-wait"
       :disabled="disabled" @click="$emit('votedFor')">
