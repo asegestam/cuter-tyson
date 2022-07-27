905870005d71b38111c6b9b25acbb57ab30dc4a8
Albin Segestam <albin.segestam@squeed.com>
2022-07-27 16:38:12 +0200

    Apply global styles to body instead of root div

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="h-full sm:h-screen flex flex-col bg-neutral-100 text-slate-700 dark:bg-slate-900 dark:text-slate-200">
+  <div>
     <NuxtLayout>
       <NuxtLoadingIndicator :height="5" color="green" />
       <SiteHeader />
